c817648a8d071ff8810bed639c86a7626c204a2a
MikhailKazaninWG <mishakazanin@gmail.com>
2022-11-04 15:03:51 +0300

    feat: configuration file added

---
+++ src/config.js
@@ -0,0 +1,14 @@
+
+const config = {
+    serverUrl: 'https://292f-5-18-162-172.eu.ngrok.io/',
+    signInPath: 'api/auth/signin/'
+}
+
+const headers = {
+    "content-type": "application/json",
+    // "Access-Control-Allow-Origin": "*",
+    // 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    // "Access-Control-Allow-Headers": "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+};
+
+export { config, headers }
